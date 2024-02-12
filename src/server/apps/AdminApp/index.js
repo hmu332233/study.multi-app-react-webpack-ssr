e799ddc3eed95ef867b902bc37b8c83ce7a9b042
Minung Han <hmu332233@gmail.com>
2024-02-12 17:28:52 +0900

    오타 수정

--- src/server/apps/AdminApp/index.js
+++ src/server/apps/AdminApp/index.js
@@ -7,9 +7,10 @@ const AdminApp = require('./App');
 const FILE_BASE_PATH = '../../../../dist/AdminApp';
 
 // 빌드된 static 파일 서빙
-router.use(express.static(path.join(__dirname, FILE_BASE_PATH, 'client'))),
-  // route
-  router.get('/admin', AdminApp.renderApp);
+router.use(express.static(path.join(__dirname, FILE_BASE_PATH, 'client')));
+
+// route
+router.get('/admin', AdminApp.renderApp);
 
 module.exports = {
   router,
